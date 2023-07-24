6fb93005674db53e8a1ccf863b754d883171aba8
vitalykolesov <metalx@list.ru>
2023-07-24 21:38:29 +0300

    начало лекции 7

---
+++ MyApp.java
@@ -0,0 +1,30 @@
+package ru.otus.java.basic.lesson7;
+
+public class MyApp {
+    public static void main(String[] args) {
+        System.out.println(sum(5; 5));
+        System.out.println(sum(15; 5));
+        System.out.println(sum(5; 125));
+    }
+
+    public static boolean isPositive(int number) {
+        if (number > 0) {
+            return number > 0;
+        }
+       /*     return true;
+        }
+        else {
+            return false;
+        }*/
+    }
+
+    public static int sum(int a, int b) {
+        int res = a + b;
+        return res;
+    }
+
+    public static int[] createPrefilledArray (int size, int initiakValue) {
+        int  out = new int[size];
+        for[i] = (int)(Math)
+    }
+}
